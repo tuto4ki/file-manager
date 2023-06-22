ee7f12be46174f63a06cee67eca826484e033fc1
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-22 15:07:49 +0300

    feat: file move done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -2,13 +2,15 @@ import { unlink } from 'fs/promises';
 import { EOL } from 'os';
 import path from 'path';
 
-export default async function remove (patFile) {
+export default async function remove (patFile, isShowMessage = true) {
     const filesName = path.join(process.cwd(), patFile);
     const errorMessage = 'FS operation failed';
 
     try {
         await unlink(filesName).then(() => {
-          console.log(`${EOL}File remove${EOL}`);
+          if (isShowMessage) {
+            console.log(`${EOL}File remove${EOL}`);
+          }
         }).catch((error) => {
             if (error.code === 'ENOENT') {
                 throw new Error(errorMessage);
